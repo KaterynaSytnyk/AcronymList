04b1dc713a0e2c45125154bd536f60e2cb5094e2
Kateryna Sytnyk <kateryna.sytnyk@gmail.com>
2015-12-16 14:09:37 -0800

    added views and custom cell files

---
+++ AcronymList/AcronymList/Views/TableViewCells/AcronymMeaningTableViewCell/AcronymMeaningTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AcronymMeaningTableViewCell.h
+//  AcronymList
+//
+//  Created by Kateryna Sytnyk on 12/16/15.
+//  Copyright © 2015 KaterynaSytnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AcronymMeaningTableViewCell : UITableViewCell
+
+@end
---
+++ AcronymList/AcronymList/Views/TableViewCells/AcronymMeaningTableViewCell/AcronymMeaningTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  AcronymMeaningTableViewCell.m
+//  AcronymList
+//
+//  Created by Kateryna Sytnyk on 12/16/15.
+//  Copyright © 2015 KaterynaSytnyk. All rights reserved.
+//
+
+#import "AcronymMeaningTableViewCell.h"
+
+@implementation AcronymMeaningTableViewCell
+
+@end
