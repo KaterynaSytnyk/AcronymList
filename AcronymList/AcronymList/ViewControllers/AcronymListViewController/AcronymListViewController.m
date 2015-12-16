9639f5aafaafc81de7f2ee5e167aa275a361f0c7
Kateryna Sytnyk <kateryna.sytnyk@gmail.com>
2015-12-16 13:51:37 -0800

    added folder structure and main files

---
+++ AcronymList/AcronymList/ViewControllers/AcronymListViewController/AcronymListViewController.m
@@ -0,0 +1,24 @@
+//
+//  AcronymListViewController.m
+//  AcronymList
+//
+//  Created by Kateryna Sytnyk on 12/16/15.
+//  Copyright © 2015 KaterynaSytnyk. All rights reserved.
+//
+
+#import "AcronymListViewController.h"
+
+@interface AcronymListViewController ()
+
+@end
+
+@implementation AcronymListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+@end
