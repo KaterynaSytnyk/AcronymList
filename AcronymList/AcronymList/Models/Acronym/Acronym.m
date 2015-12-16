f290102c255c2859f53c8098ff648e5d99709f87
Kateryna Sytnyk <kateryna.sytnyk@gmail.com>
2015-12-16 14:06:38 -0800

    added models

---
+++ AcronymList/AcronymList/Models/Acronym/Acronym.m
@@ -0,0 +1,13 @@
+//
+//  Acronym.m
+//  AcronymList
+//
+//  Created by Kateryna Sytnyk on 12/16/15.
+//  Copyright © 2015 KaterynaSytnyk. All rights reserved.
+//
+
+#import "Acronym.h"
+
+@implementation Acronym
+
+@end
---
+++ AcronymList/AcronymList/Models/AcronymMeaning/AcronymMeaning.m
@@ -0,0 +1,13 @@
+//
+//  AcronymMeaning.m
+//  AcronymList
+//
+//  Created by Kateryna Sytnyk on 12/16/15.
+//  Copyright © 2015 KaterynaSytnyk. All rights reserved.
+//
+
+#import "AcronymMeaning.h"
+
+@implementation AcronymMeaning
+
+@end
