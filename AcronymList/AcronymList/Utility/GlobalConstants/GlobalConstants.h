c61fb6da66b147b6687f3dee15eff31ff449f1d4
Kateryna Sytnyk <kateryna.sytnyk@gmail.com>
2015-12-16 15:47:22 -0800

    added global constants

---
+++ AcronymList/AcronymList/Utility/GlobalConstants/GlobalConstants.h
@@ -0,0 +1,17 @@
+//
+//  GlobalConstants.h
+//  AcronymList
+//
+//  Created by Kateryna Sytnyk on 12/16/15.
+//  Copyright © 2015 KaterynaSytnyk. All rights reserved.
+//
+
+#ifndef GlobalConstants_h
+#define GlobalConstants_h
+
+typedef void(^DefaultCompletionBlock)(void);
+typedef void(^DefaultSuccessHandler)(void);
+typedef void(^DefaultErrorHandler)(NSString *localizedErrorMessage);
+
+
+#endif /* GlobalConstants_h */
